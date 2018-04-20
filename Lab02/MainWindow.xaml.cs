fc1a10db8a76fe9f35db416aadb1386703b03094
Gringger <32059567+Gringger@users.noreply.github.com>
2018-04-21 02:53:10 +0300

    Lab02

---
+++ Lab02/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using CSharpLab02;
+using FontAwesome.WPF;
+
+namespace Lab02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ImageAwesome _loader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new AddPersonView(ShowLoader);
+        }
+
+        private void ShowLoader(bool isShow)
+        {
+            LoaderHelper.OnRequestLoader(MainGrid, ref _loader, isShow);
+        }
+    }
+}
